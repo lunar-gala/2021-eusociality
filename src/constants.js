12f1ca07f9e95902585d513d055e46329f9c3ead
mikinty <youmichaelc@gmail.com>
2020-11-25 18:56:35 -0500

    model with mouse intreaction

--- src/constants.js
+++ src/constants.js
@@ -7,3 +7,14 @@ export const NAVBAR_LIST = [
   'BEHIND THE SCENES',
   'PEOPLE'
 ];
+
+/** @brief Default camera position */
+export const CAMERA_POSITION = {
+  x: 300,
+  y: 300,
+  z: 300
+};
+
+/** @brief */
+export const CAMERA_PAN_X_FACTOR = 30;
+export const CAMERA_PAN_Y_FACTOR = 30;
