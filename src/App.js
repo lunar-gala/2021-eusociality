751df13e8aed9004ce993e98ad0c032b3ec17227
mikinty <youmichaelc@gmail.com>
2020-12-27 15:47:46 -0500

    Use hashrouter instead

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import LandingPage from './pages/LandingPage';
 import AboutPage from './pages/AboutPage';
 import PeoplePage from './pages/PeoplePage';
