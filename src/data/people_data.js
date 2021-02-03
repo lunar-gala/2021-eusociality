5c2606dce9147866a9e36f6fef332364d60dfea7
mikinty <youmichaelc@gmail.com>
2021-02-03 04:15:30 -0500

    Created people data json by combining spreadsheets

---
+++ src/data/people_data.js
@@ -0,0 +1,676 @@
+/**
+ * Data for people involved in LG, what team they belong to, preferred names,
+ * and headshot files.
+ */
+
+/**
+ * @brief Pretty self explanatory json formatted data of people. People without
+ * headshots are indicated with a null filename.
+ */
+export const PEOPLE_DATA = {
+  "scarpent": {
+    "first_name": "Sebastian",
+    "last_name": "Carpenter",
+    "team": "Creative",
+    "position": "Creative Director",
+    "filename": "Sebastian Carpenter.jpg"
+  },
+  "wjiao": {
+    "first_name": "Mimi",
+    "last_name": "Jiao",
+    "team": "Creative",
+    "position": "Creative Director",
+    "filename": "Weiyin Jiao.jpg"
+  },
+  "szhai": {
+    "first_name": "Sabrina",
+    "last_name": "Zhai",
+    "team": "Creative",
+    "position": "Head of Web",
+    "filename": "Sabrina Zhai.jpg"
+  },
+  "jsaik": {
+    "first_name": "Jaclyn ",
+    "last_name": "Saik",
+    "team": "Creative",
+    "position": "Web Designer",
+    "filename": "Jaclyn Saik.jpg"
+  },
+  "jeeyoonk": {
+    "first_name": "Jenna",
+    "last_name": "Kim",
+    "team": "Creative",
+    "position": "Web Designer",
+    "filename": "Jeeyoon Kim.jpg"
+  },
+  "sophiaki": {
+    "first_name": "Sophia",
+    "last_name": "Kim",
+    "team": "Creative",
+    "position": "Web Designer",
+    "filename": "Sophia Kim.jpg"
+  },
+  "rickyche": {
+    "first_name": "Ricky ",
+    "last_name": "Chen",
+    "team": "Creative",
+    "position": "Web Designer",
+    "filename": "Ricky Chen.jpg"
+  },
+  "myou": {
+    "first_name": "Michael",
+    "last_name": "You",
+    "team": "Creative",
+    "position": "Web Developer",
+    "filename": "Michael You.jpg"
+  },
+  "rxun": {
+    "first_name": "Ruiran",
+    "last_name": "Xun",
+    "team": "Creative",
+    "position": "Web Developer",
+    "filename": "Ruiran Xun.jpg"
+  },
+  "hpnguyen": {
+    "first_name": "Phi",
+    "last_name": "Nguyen",
+    "team": "Creative",
+    "position": "Web Developer",
+    "filename": "Henry Nguyen.jpg"
+  },
+  "changjuc": {
+    "first_name": "Danny",
+    "last_name": "Cho",
+    "team": "Creative",
+    "position": "Head of Motion",
+    "filename": "Chang Jun Cho.jpg"
+  },
+  "flg": {
+    "first_name": "Franklin",
+    "last_name": "Gutman",
+    "team": "Creative",
+    "position": "Motion Designer",
+    "filename": null
+  },
+  "hycai": {
+    "first_name": "Hannah",
+    "last_name": "Cai",
+    "team": "Creative",
+    "position": "Motion Designer",
+    "filename": "Hannah Cai.jpg"
+  },
+  "sammiek": {
+    "first_name": "Sammie",
+    "last_name": "Kim",
+    "team": "Creative",
+    "position": "Motion Designer",
+    "filename": "Sammie Kim.jpg"
+  },
+  "yrt": {
+    "first_name": "Yoshi",
+    "last_name": "Torralva",
+    "team": "Creative",
+    "position": "Motion Designer",
+    "filename": "Yoshua Torralva.jpg"
+  },
+  "eileenl1": {
+    "first_name": "Eileen",
+    "last_name": "Lee",
+    "team": "Creative",
+    "position": "Motion Designer",
+    "filename": null
+  },
+  "elizabew": {
+    "first_name": "Elizabeth",
+    "last_name": "Wang",
+    "team": "Creative",
+    "position": "Motion Designer",
+    "filename": "Elizabeth Wang.jpg"
+  },
+  "ahl2": {
+    "first_name": "Angela",
+    "last_name": "Lee",
+    "team": "Creative",
+    "position": "Head of Print",
+    "filename": "Angela Lee.jpg"
+  },
+  "alcai": {
+    "first_name": "Alice",
+    "last_name": "Cai",
+    "team": "Creative",
+    "position": "Print Designer",
+    "filename": null
+  },
+  "ehyang": {
+    "first_name": "Ethan",
+    "last_name": "Yang",
+    "team": "Creative",
+    "position": "Print Designer",
+    "filename": null
+  },
+  "ertsai": {
+    "first_name": "Elysha",
+    "last_name": "Tsai",
+    "team": "Creative",
+    "position": "Print Designer",
+    "filename": null
+  },
+  "chsong": {
+    "first_name": "Caroline",
+    "last_name": "Song",
+    "team": "Creative",
+    "position": "Print Designer",
+    "filename": "Caroline Song.jpg"
+  },
+  "puxins": {
+    "first_name": "Clytze",
+    "last_name": "Sun",
+    "team": "Creative",
+    "position": "Head of Photo",
+    "filename": "Clytze Sun.jpg"
+  },
+  "ddomalik": {
+    "first_name": "David",
+    "last_name": "Domalik",
+    "team": "Creative",
+    "position": "Photographer",
+    "filename": "David Domalik.jpg"
+  },
+  "mcagan": {
+    "first_name": "Melissa ",
+    "last_name": "Cagan",
+    "team": "Creative",
+    "position": "Photographer",
+    "filename": null
+  },
+  "cmay1": {
+    "first_name": "CJ",
+    "last_name": "May",
+    "team": "Creative",
+    "position": "Photographer",
+    "filename": "Christopher May.jpg"
+  },
+  "iyip": {
+    "first_name": "Iris",
+    "last_name": "Yip",
+    "team": "Creative",
+    "position": "Photographer",
+    "filename": null
+  },
+  "vderienz": {
+    "first_name": "Vincent",
+    "last_name": "DeRienzo",
+    "team": "Design",
+    "position": "Head Design Coordinator",
+    "filename": "Vincent DeRienzo.jpg"
+  },
+  "aburbano": {
+    "first_name": "Ashley",
+    "last_name": "Burbano",
+    "team": "Design",
+    "position": "Design Coordinator",
+    "filename": null
+  },
+  "seyoungc": {
+    "first_name": "Seyoung",
+    "last_name": "Choo",
+    "team": "Design",
+    "position": "Design Coordinator",
+    "filename": null
+  },
+  "mpandura": {
+    "first_name": "Maya",
+    "last_name": "Pandurangan",
+    "team": "Design",
+    "position": "Obfuscate ",
+    "filename": "Maya Pandurangan.jpg"
+  },
+  "abhartia": {
+    "first_name": "Aadya ",
+    "last_name": "Bhartia",
+    "team": "Design",
+    "position": "Renasci",
+    "filename": "Aadya Bhartia.jpg"
+  },
+  "lenal": {
+    "first_name": "Lena",
+    "last_name": "Li",
+    "team": "Design",
+    "position": "Heroine",
+    "filename": "Lena Li.jpg"
+  },
+  "zhehuil": {
+    "first_name": "Linda",
+    "last_name": "Lin",
+    "team": "Design",
+    "position": "Heroine",
+    "filename": "Zhehui Lin.jpg"
+  },
+  "odh": {
+    "first_name": "Owen",
+    "last_name": "Haft",
+    "team": "Design",
+    "position": "Bound",
+    "filename": "Owen Haft.jpg"
+  },
+  "carmeny": {
+    "first_name": "Carmen",
+    "last_name": "Yu",
+    "team": "Design",
+    "position": "Bound",
+    "filename": "Carmen Yu.jpg"
+  },
+  "abandi": {
+    "first_name": "Arushi",
+    "last_name": "Bandi",
+    "team": "Design",
+    "position": "Obfuscate",
+    "filename": "Arushi Bandi.jpg"
+  },
+  "knandgao": {
+    "first_name": "Khushi",
+    "last_name": "Nanddgaonkar",
+    "team": "Design",
+    "position": "Mirror",
+    "filename": "Khushi Nandgaonkar.jpg"
+  },
+  "kathyz": {
+    "first_name": "Kathy",
+    "last_name": "Zhang",
+    "team": "Design",
+    "position": "Mirror",
+    "filename": "Kathy Zhang.jpg"
+  },
+  "skylarm ": {
+    "first_name": "Skylar",
+    "last_name": "McAuliffe",
+    "team": "Design",
+    "position": "The Marianas Trench",
+    "filename": null
+  },
+  "ajafrani": {
+    "first_name": "Amal ",
+    "last_name": "Jafrani",
+    "team": "Design",
+    "position": "Bloom",
+    "filename": "Amal Jafrani.jpg"
+  },
+  "jlai2": {
+    "first_name": "Jessica",
+    "last_name": "Lai",
+    "team": "Design",
+    "position": "Gossemer Idle",
+    "filename": "Jessica Lai.jpg"
+  },
+  "ejchapma": {
+    "first_name": "Elise",
+    "last_name": "Chapman",
+    "team": "Design",
+    "position": "Gossemer Idle",
+    "filename": "Elise Chapman.jpg"
+  },
+  "nwaldram": {
+    "first_name": "Natalie",
+    "last_name": "Waldram",
+    "team": "Design",
+    "position": "Bloom",
+    "filename": "Natalie Waldram.jpg"
+  },
+  "ihorgan": {
+    "first_name": "Iz",
+    "last_name": "Horgan",
+    "team": "Design",
+    "position": "Chrysophyta",
+    "filename": "Isabel Horgan.jpg"
+  },
+  "apao": {
+    "first_name": "Annalisa",
+    "last_name": "Pao",
+    "team": "Design",
+    "position": "Nexus",
+    "filename": "Annalisa Pao.jpg"
+  },
+  "yinjiet": {
+    "first_name": "Jacky",
+    "last_name": "Tian",
+    "team": "Design",
+    "position": "Relinquo",
+    "filename": "Jacky Tian.jpg"
+  },
+  "minghaoy": {
+    "first_name": "Kwins",
+    "last_name": "Yang",
+    "team": "Design",
+    "position": "Relinquo",
+    "filename": "Minghao Yang.jpg"
+  },
+  "lkendric": {
+    "first_name": "Leah",
+    "last_name": "Kendrick",
+    "team": "Design",
+    "position": "Icarus",
+    "filename": "Leah Kendrick.jpg"
+  },
+  "hsiaotyp": {
+    "first_name": "Tyng Peck",
+    "last_name": "Peck",
+    "team": "Design",
+    "position": "Icarus",
+    "filename": null
+  },
+  "jwesthei": {
+    "first_name": "Jessa",
+    "last_name": "Westheimer",
+    "team": "Design",
+    "position": "Somatic",
+    "filename": ""
+  },
+  "oluk": {
+    "first_name": "Olivia Luk",
+    "last_name": "Luk",
+    "team": "Design",
+    "position": "Invation",
+    "filename": "Olivia Luk.jpg"
+  },
+  "cscheire": {
+    "first_name": "Cassie",
+    "last_name": "Scheirer",
+    "team": "Design",
+    "position": "Digital Warfare",
+    "filename": null
+  },
+  "madelind": {
+    "first_name": "Madi",
+    "last_name": "Davis",
+    "team": "Design",
+    "position": "Digital Warfare",
+    "filename": null
+  },
+  "adriennw": {
+    "first_name": "Adrienne",
+    "last_name": "Wang",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Adrienne Wang.jpg"
+  },
+  "aestuart": {
+    "first_name": "Audry",
+    "last_name": "Stuart",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Audry Stuart.jpg"
+  },
+  "lingfeng": {
+    "first_name": "Bill",
+    "last_name": "Gao",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Lingfeng Gao.jpg"
+  },
+  "csinagra": {
+    "first_name": "Chloe",
+    "last_name": "Sinagra",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Chloe Sinagra.jpg"
+  },
+  "christi5": {
+    "first_name": "Christina",
+    "last_name": "Li",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Christina Li.jpg"
+  },
+  "cmedward": {
+    "first_name": "Clayton",
+    "last_name": "Edwards",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Clayton Edwards.jpg"
+  },
+  "dgardilc": {
+    "first_name": "Danova",
+    "last_name": "Gardilcic",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Danova Gardilcic.jpg"
+  },
+  "dongwonl": {
+    "first_name": "Dong Won (Don)",
+    "last_name": "Lee",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Dong Won Lee.jpg"
+  },
+  "fpd": {
+    "first_name": "Fiona",
+    "last_name": "Dubrosa",
+    "team": "Model",
+    "position": "Model Coordinator",
+    "filename": null
+  },
+  "xhe2": {
+    "first_name": "Ivy",
+    "last_name": "He",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Xiao He.jpg"
+  },
+  "jlangan": {
+    "first_name": "Jade",
+    "last_name": "Langan",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Jade Langan.jpg"
+  },
+  "jtimczyk": {
+    "first_name": "Jessica",
+    "last_name": "Timczyk",
+    "team": "Model",
+    "position": "Model, Producer",
+    "filename": "Jessica Timczyk.jpg"
+  },
+  "minjael2": {
+    "first_name": "Julia",
+    "last_name": "Lee",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Min Jae Lee.jpg"
+  },
+  "kbalenza": {
+    "first_name": "Kyra",
+    "last_name": "Balenzano",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Kyra Balenzano.jpg"
+  },
+  "lgunasek": {
+    "first_name": "Lahiru",
+    "last_name": "Gunasekara",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Lahiru Gunasekara.jpg"
+  },
+  "mcornwal": {
+    "first_name": "Lindy",
+    "last_name": "Cornwall",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Melinda Cornwall.jpg"
+  },
+  "laltus": {
+    "first_name": "Lucy",
+    "last_name": "Altus",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Lucy Altus.jpg"
+  },
+  "mgamerma": {
+    "first_name": "Max",
+    "last_name": "Gamerman",
+    "team": "Model",
+    "position": "Model Coordinator",
+    "filename": "Max Gamerman.jpg"
+  },
+  "maxliu": {
+    "first_name": "Max",
+    "last_name": "Liu",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Max Liu.jpg"
+  },
+  "ruomingx": {
+    "first_name": "Ming",
+    "last_name": "Xin",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Ruoming Xin.jpg"
+  },
+  "makim": {
+    "first_name": "Minseo (Angelica)",
+    "last_name": "Kim",
+    "team": "Model",
+    "position": "Model",
+    "filename": null
+  },
+  "morganbo": {
+    "first_name": "Morgan",
+    "last_name": "Boyd",
+    "team": "Model",
+    "position": "Model",
+    "filename": null
+  },
+  "njeans": {
+    "first_name": "Nathalie",
+    "last_name": "Jeans",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Nathalie Jeans.jpg"
+  },
+  "nathanwa": {
+    "first_name": "Nathan",
+    "last_name": "Wai",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Nathan Wai.jpg"
+  },
+  "nwai": {
+    "first_name": "Nora",
+    "last_name": "Wai",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Nora Wai.jpg"
+  },
+  "ojinsi": {
+    "first_name": "Omisa",
+    "last_name": "Jinsi",
+    "team": "Model",
+    "position": "Model",
+    "filename": "omisa jinsi.jpg"
+  },
+  "pgao1": {
+    "first_name": "Patrick",
+    "last_name": "Gao",
+    "team": "Model",
+    "position": "Model",
+    "filename": null
+  },
+  "pqin": {
+    "first_name": "Pauline",
+    "last_name": "Qin",
+    "team": "Model",
+    "position": "Head Model Coordinator",
+    "filename": "Pauline Qin.jpg"
+  },
+  "rkoanda": {
+    "first_name": "Rahjshiba",
+    "last_name": "Koanda",
+    "team": "Model",
+    "position": "Model",
+    "filename": ""
+  },
+  "radeoti": {
+    "first_name": "Remi",
+    "last_name": "Adeoti",
+    "team": "Model",
+    "position": "Model Coordinator",
+    "filename": null
+  },
+  "sammiep": {
+    "first_name": "Sammie",
+    "last_name": "Paul",
+    "team": "Model",
+    "position": "Model",
+    "filename": null
+  },
+  "sophiale": {
+    "first_name": "Sophia",
+    "last_name": "Lee",
+    "team": "Model",
+    "position": "Model",
+    "filename": ""
+  },
+  "tbournia": {
+    "first_name": "Themistokles",
+    "last_name": "Bournias",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Themistokles Bournias.jpg"
+  },
+  "thk1": {
+    "first_name": "Tiffany ",
+    "last_name": "Kim",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Tiffany Kim.jpg"
+  },
+  "whliu": {
+    "first_name": "Wilford",
+    "last_name": "Liu",
+    "team": "Model",
+    "position": "Model",
+    "filename": "Hongfu Liu.jpg"
+  },
+  "mkimkim": {
+    "first_name": "Aris",
+    "last_name": "Kim",
+    "team": "Public Relations",
+    "position": "Head of PR",
+    "filename": "Min Ji Kim Kim.jpg"
+  },
+  "lannal": {
+    "first_name": "Lanna",
+    "last_name": "Lang",
+    "team": "Public Relations",
+    "position": "Social Media Manager",
+    "filename": "Lanna Lang.jpg"
+  },
+  "sspektor": {
+    "first_name": "Sabina",
+    "last_name": "Spektor",
+    "team": "Public Relations",
+    "position": "Sponsorship Liaison",
+    "filename": "Sabina Spektor.jpg"
+  },
+  "ejc2": {
+    "first_name": "Elizabeth",
+    "last_name": "Choi",
+    "team": "Public Relations",
+    "position": "Sponsorship Liaison",
+    "filename": "Elizabeth Choi.jpg"
+  },
+  "patriciy": {
+    "first_name": "Patricia",
+    "last_name": "Yu",
+    "team": "Design",
+    "position": "Duration",
+    "filename": "Patricia Yu.jpg"
+  },
+  "loberman": {
+    "first_name": "Lily ",
+    "last_name": "Oberman",
+    "team": "Design",
+    "position": "The Marianas Trench",
+    "filename": ""
+  }
+}
