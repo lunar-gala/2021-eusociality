f032ab1b187188aa793eab632eb9ae0852a9620c
mikinty <youmichaelc@gmail.com>
2021-02-15 14:26:00 -0500

    src/pages linting

--- src/pages/AboutPageDesktop.js
+++ src/pages/AboutPageDesktop.js
@@ -1,28 +1,27 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-import * as CONSTANTS from '../constants';
+import * as CONSTANTS from "../constants";
 
 class AboutPageDesktop extends React.Component {
   render() {
     return (
-      <div id='about-page-desktop' className={`desktop ${this.props.landing_page_state}`}>
-        <div id='headline'>
-          <div id='headline-text'>
-            {CONSTANTS.ABOUT_HEADER_TEXT}
-          </div>
-          <div id='right-bar'>
-            <div className='dot-basic'/>
-            <div className='line'/>
+      <div
+        id="about-page-desktop"
+        className={`desktop ${this.props.landing_page_state}`}
+      >
+        <div id="headline">
+          <div id="headline-text">{CONSTANTS.ABOUT_HEADER_TEXT}</div>
+          <div id="right-bar">
+            <div className="dot-basic" />
+            <div className="line" />
           </div>
         </div>
-        <div id='bottom-descriptor'>
-          <div id='left-bar'>
-            <div className='dot-basic'/>
-            <div className='line'/>
+        <div id="bottom-descriptor">
+          <div id="left-bar">
+            <div className="dot-basic" />
+            <div className="line" />
           </div>
-          <div id='description'>
-            {CONSTANTS.ABOUT_TEXT}
-          </div>
+          <div id="description">{CONSTANTS.ABOUT_TEXT}</div>
         </div>
       </div>
     );
--- src/pages/AboutPageMobile.js
+++ src/pages/AboutPageMobile.js
@@ -2,37 +2,41 @@
  * About page for mobile
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import * as CONSTANTS from '../constants';
+import React from "react";
+import PropTypes from "prop-types";
+import * as CONSTANTS from "../constants";
 
 class AboutPageMobile extends React.Component {
   render() {
-    return <div id='about-page-mobile' className={this.props.landing_page_state + ' mobile'}>
-      <div id='header-section'>
-        <div id='right-bar' className='bar'>
-          <div className='dot-basic' />
-          <div className='line' />
+    return (
+      <div
+        id="about-page-mobile"
+        className={this.props.landing_page_state + " mobile"}
+      >
+        <div id="header-section">
+          <div id="right-bar" className="bar">
+            <div className="dot-basic" />
+            <div className="line" />
+          </div>
+          <div id="header-text" className="text-box">
+            {CONSTANTS.ABOUT_HEADER_TEXT}
+          </div>
+          <div id="left-bar" className="bar">
+            <div className="line" />
+            <div className="dot-basic" />
+          </div>
         </div>
-        <div id='header-text' className='text-box'>
-          {CONSTANTS.ABOUT_HEADER_TEXT}
-        </div>
-        <div id='left-bar' className='bar'>
-          <div className='line' />
-          <div className='dot-basic' />
+        <div id="main-text" className="text-box">
+          {CONSTANTS.ABOUT_TEXT}
         </div>
       </div>
-      <div id='main-text' className='text-box'>
-        {CONSTANTS.ABOUT_TEXT}
-      </div>
-    </div>
+    );
   }
 }
 
 AboutPageMobile.propTypes = {
   /** @brief Indicates if the menu is open or not, controlled by the parent */
-  landing_page_state: PropTypes.string.isRequired
-}
+  landing_page_state: PropTypes.string.isRequired,
+};
 
 export default AboutPageMobile;
-
