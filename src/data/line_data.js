bfe58409434c5e99acddace01b9c98243284cb7b
mikinty <youmichaelc@gmail.com>
2021-03-06 22:39:26 -0500

    Added videos to the line page, with the react player package

--- src/data/line_data.js
+++ src/data/line_data.js
@@ -2,6 +2,8 @@
  * Contains data for all the lines.
  */
 
+export const GITHUB_BASE_URL = 'https://github.com/lunar-gala/2021-eusociality-videos/raw/master/';
+
 /** @brief Line names in order, with their respective designers */
 export const LINE_INFO = [
   {
@@ -10,7 +12,7 @@ export const LINE_INFO = [
       'Elise Chapman',
       'Jessica Lai'
     ],
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus iaculis leo, quis mollis ligula ultrices condimentum. Donec sagittis, sem vitae scelerisque porta, eros libero volutpat orci, quis placerat sapien lacus at quam. Quisque maximus vel enim sit amet blandit. Curabitur tempus nisl a ex aliquet, et lobortis metus rhoncus. Vestibulum sit amet lobortis augue, viverra feugiat nisi. Ut in ultrices risus. Morbi placerat diam semper dapibus rhoncus. Nam ut convallis dui. Praesent dictum erat tincidunt sapien molestie, sed rhoncus est posuere. Morbi placerat diam semper dapibus rhoncus. Nam ut convallis dui. Praesent dictum erat tincidunt sapien molestie, sed rhoncus.'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus iaculis leo, quis mollis ligula ultrices condimentum. Donec sagittis, sem vitae scelerisque porta, eros libero volutpat orci, quis placerat sapien lacus at quam. Quisque maximus vel enim sit amet blandit. Curabitur tempus nisl a ex aliquet, et lobortis metus rhoncus. Vestibulum sit amet lobortis augue, viverra feugiat nisi. Ut in ultrices risus. Morbi placerat diam semper dapibus rhoncus. Nam ut convallis dui. Praesent dictum erat tincidunt sapien molestie, sed rhoncus est posuere. Morbi placerat diam semper dapibus rhoncus. Nam ut convallis dui. Praesent dictum erat tincidunt sapien molestie, sed rhoncus.',
   },
   {
     name: 'Lorelei',
@@ -23,7 +25,8 @@ export const LINE_INFO = [
     name: 'Nexus',
     designers: [
       'Annalisa Pao'
-    ]
+    ],
+    video_ready: 'https://streamable.com/9furjq',
   },
   {
     name: 'Chrysophyta',
@@ -43,20 +46,22 @@ export const LINE_INFO = [
     designers: [
       'Lily Oberman',
       'Skylar McAuliffe'
-    ]
+    ],
+    video_ready: 'https://streamable.com/zq5rhe',
   },
   {
     name: 'Obfuscate',
     designers: [
       'Arushi Bandi',
       'Maya Pandagurun'
-    ]
+    ],
+    video_ready: 'https://streamable.com/snrgd8',
   },
   {
     name: 'Salvos',
     designers: [
       'Patricia Yu'
-    ]
+    ],
   },
   {
     name: 'Bloom',
@@ -76,33 +81,36 @@ export const LINE_INFO = [
     designers: [
       'Owen Haft',
       'Carmen Yu',
-    ]
+    ],
+    video_ready: 'https://streamable.com/bnk0lt',
   },
   {
     name: 'Invasion',
     designers: [
       'Olivia Luk',
-    ]
+    ],
+    video_ready: 'https://streamable.com/6gtby0',
   },
   {
     name: 'Somatic',
     designers: [
       'Jessa Westheimer'
-    ]
+    ],
+    video_ready: 'https://streamable.com/ssifa5',
   },
   {
     name: 'Relinquo',
     designers: [
       'Kwins Yang',
       'Jacky Tian'
-    ]
+    ],
   },
   {
     name: 'Digital Warfare',
     designers: [
       'Cassie Scheirer',
       'Madi Davis'
-    ]
+    ],
   },
   {
     name: 'Icarus',
