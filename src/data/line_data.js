069f3c2fbec0eee82d744340cccb9f8440eca0d6
zhaisabrina <szhai@andrew.cmu.edu>
2021-03-16 17:19:24 -0400

    Line data
    Updated all videos + line descriptions

--- src/data/line_data.js
+++ src/data/line_data.js
@@ -12,6 +12,7 @@ export const LINE_INFO = [
       'Elise Chapman',
       'Jessica Lai'
     ],
+    video_ready: 'https://streamable.com/9z91c9',
     description: 'Gossamer Idle is a combination between the grandeur of the Renaissance and modern day elegance. This idea stems from our luna moth motif — representing the renewal of body and spirit regeneration and ties in to our intent to create a new version of an old trend. With flowy and translucent material paired with harsher metals and leather, we create a sense of duality between femininity and masculinity in each look. Instead of following the distinct binary roles from old Europe, we believe that fashion is not a performance but performative, and we can use fashion to undermine gender stereotypes.',
   },
   {
@@ -21,12 +22,14 @@ export const LINE_INFO = [
     ],
     description: 'Regardless of gender identity, hardness and softness coexist in each of us through masculine and feminine energy. The looks in Nexus embrace the varying balances of hardness and softness in people but also work to challenge the constructed ideas of traditional masculinity and femininity.',
     video_ready: 'https://streamable.com/9furjq',
+    description :'Regardless of gender identity, hardness and softness coexist in each of us through masculine and feminine energy. The looks in Nexus embrace the varying balances of hardness and softness in people but also work to challenge the constructed ideas of traditional masculinity and femininity.',
   },
   {
     name: 'Chrysophyta',
     designers: [
       'Iz Horgan'
     ],
+    video_ready: 'https://streamable.com/vn4mnz',
     description: 'Chrysophyta (kris-aw-fi-ta) is a golden colored algae found mostly in freshwater environments. With this reference in mind, the line merges classic dress silhouettes with the generative quality of this natural form. Layers of gathered and pleated chiffon are utilized in each garment to add movement, line and color.',
   },
   {
@@ -35,6 +38,7 @@ export const LINE_INFO = [
       'Khushi Nandgoankar',
       'Kathy Zhang'
     ],
+    video_ready: 'https://streamable.com/zaclht',
     description: 'Mirror depicts Pittsburgh’s various textured spaces through eight patchworked looks. Each look presents a different area of the city through either a landscape or an overhead view. There is often a disconnect between CMU students and the rest of the Pittsburgh community. Many undergrads leave Pittsburgh after four years having only seen a handful of its ninety neighborhoods. Pittsburgh is filled with much history and culture. It is a city teeming with talent and passionate community leaders. We hope that by depicting Pittsburgh through patchwork, we can inspire a deeper love and appreciation for the city within the CMU community.',
   },
   {
@@ -44,6 +48,7 @@ export const LINE_INFO = [
       'Skylar McAuliffe'
     ],
     video_ready: 'https://streamable.com/zq5rhe',
+    description: 'As humans, we pursue the unknown. The Marianas Trench is the deepest oceanic trench on Earth, perpetually dark and near freezing. It is a site completely distinct from human life, yet it continues to draw people down to extreme depths solely for the purpose of exploring the unexplored. Those who venture there bring back proof of the biodiversity, color, and beauty that thrives in the extreme pressure and temperature at these depths. Our line is a testament to the power of curiosity and exploration, through which we can bring light and warmth to the furthest mysteries of the universe.',
   },
   {
     name: 'Obfuscate',
@@ -51,16 +56,16 @@ export const LINE_INFO = [
       'Arushi Bandi',
       'Maya Pandagurun'
     ],
-    description: 'As humans, we pursue the unknown. The Marianas Trench is the deepest oceanic trench on Earth, perpetually dark and near freezing. It is a site completely distinct from human life, yet it continues to draw people down to extreme depths solely for the purpose of exploring the unexplored. Those who venture there bring back proof of the biodiversity, color, and beauty that thrives in the extreme pressure and temperature at these depths. Our line is a testament to the power of curiosity and exploration, through which we can bring light and warmth to the furthest mysteries of the universe.',
-    video_ready: 'https://streamable.com/snrgd8',
+    video_ready: 'https://streamable.com/79rktz',
+    description: 'Surveillance has become ubiquitous in our daily life. We are watched whenever we leave our homes, wherever we go. Some activists and artists have even begun to wear “anti-surveillance” masks, accessories and make-up. We take this one step further and imagine a world where everyone takes precautions against the omnipresent eye of the camera through fashion. We explore how wealth will play a factor in who is seen and who goes by unnoticed. We include bright colors and dizzying patterns suited to confuse the camera and obfuscate people.This line is a humorous and chilling take on our further narrowing circle of privacy.',
   },
   {
     name: 'Salvos',
     designers: [
       'Patricia Yu'
     ],
-    description: 'Different reactions were taken towards a shared experience recently. Whether it was advantageous or a catastrophe, Salvos embodies this moment in history - each costume reflects methods and values of “survival” that people have taken. The entropy of the pursuit of survival in action is intense, and may seem counterintuitive when seen individually. But when together, the spirit is strong and harmonious, giving hope to new beginnings. ',
-    video_ready: 'https://streamable.com/fjsba8',
+    video_ready: 'https://streamable.com/zaclht',
+    description: 'Different reactions were taken towards a shared experience recently. Whether it was advantageous or a catastrophe, Salvos embodies this moment in history - each costume reflects methods and values of “survival” that people have taken. The entropy of the pursuit of survival in action is intense, and may seem counterintuitive when seen individually. But when together, the spirit is strong and harmonious, giving hope to new beginnings.',
   },
   {
     name: 'Bloom',
@@ -68,6 +73,7 @@ export const LINE_INFO = [
       'Natalie Waldram',
       'Amal Jafrani'
     ],
+    video_ready: 'https://streamable.com/rxr4gk',
     description: 'Bloom inspired by the uninhibited expression of childhood. Childhood can be characterized as a moment of intense curiosity as well as a vivid exploration of personal and cultural identity, which can soften as we age due to stricter adherence to social norms, standards, and occasions. Bloom aims to recreate the imaginative identities that emerge from a child’s extravagant, fantasy world by creating aesthetic abstractions of fictional childhood obsessions. This line combines a child’s disregard for time and place with a fascination for fantasy to create “occasion-less”, fantasy-inspired pieces.',
   },
   {
@@ -75,6 +81,7 @@ export const LINE_INFO = [
     designers: [
       'Aadya Bhartia',
     ],
+    video_ready: 'https://streamable.com/hgs89l',
     description: 'Renasci explores the interaction between growth and rebirth. Adaptability and evolution work in unison as we traverse through various phases of our existence. The grounds of this evolution is determined by the environment we thrive in. Through Renasci, I aim to draw a parallel between the malleability of wire and the influence of the people who surround us. The delicate fabric contrasts against the rigid wire, creating a dialect between the two and imbibes an instance of rebirth.',
   },
   {
@@ -85,6 +92,7 @@ export const LINE_INFO = [
     ],
     description: 'Bound juxtaposes the preconceptions of chains and metal with the lightness and transparency of a veil. Metal and chains are assumed as rigid and restrictive. We seek to counter that view by demonstrating an exaggerated form of and around the human body by keeping a sense of lightness to the garments, through the use of handmade metal chains, metallic wire, and black fabrics that accentuate some, while covering other parts of the human form. We aim to create beautifully embellished garments that evoke and challenge the seriousness that comes with metal chains. ',
     video_ready: 'https://streamable.com/bnk0lt',
+    description: 'Bound juxtaposes the preconceptions of chains and metal with the lightness and transparency of a veil. Metal and chains are assumed as rigid and restrictive. We seek to counter that view by demonstrating an exaggerated form of and around the human body by keeping a sense of lightness to the garments, through the use of handmade metal chains, metallic wire, and black fabrics that accentuate some, while covering other parts of the human form. We aim to create beautifully embellished garments that evoke and challenge the seriousness that comes with metal chains.',
   },
   {
     name: 'Invasion',
@@ -92,7 +100,7 @@ export const LINE_INFO = [
       'Olivia Luk',
     ],
     video_ready: 'https://streamable.com/6gtby0',
-    description: 'Lately there have been a lot of changes my life that I feel are outside of my control and when I try to resist these changes it feels like I’m trying to stop the inevitable. Inspired by how the human body and natural world reflect this sense of invasive growth, I took images of things people often try to, but can’t control about the human body like acne, body hair, and bodily fluids, and tried to relate them to images of unwanted plants and species in nature like mold, sea slugs, algae, ivy and fungi. ',
+    description: 'Lately there have been a lot of changes my life that I feel are outside of my control and when I try to resist these changes it feels like I’m trying to stop the inevitable. Inspired by how the human body and natural world reflect this sense of invasive growth, I took images of things people often try to, but can’t control about the human body like acne, body hair, and bodily fluids, and tried to relate them to images of unwanted plants and species in nature like mold, sea slugs, algae, ivy and fungi.',
   },
   {
     name: 'Somatic',
@@ -101,6 +109,7 @@ export const LINE_INFO = [
     ],
     description: 'Somatic considers the relationship we have with our bodies, especially with what is beneath our skin. It is important to reflect on the way we are designed as well as the structures, systems, and forms  which sustain us. We are not at all the Vitruvian Man or an illustration in an anatomy textbook or a Photoshopped photo in a magazine. We are imperfect and beautiful, and somehow, all the parts of our bodies keep us moving and living.',
     video_ready: 'https://streamable.com/ssifa5',
+    description: 'Somatic considers the relationship we have with our bodies, especially with what is beneath our skin. It is important to reflect on the way we are designed as well as the structures, systems, and forms  which sustain us. We are not at all the Vitruvian Man or an illustration in an anatomy textbook or a Photoshopped photo in a magazine. We are imperfect and beautiful, and somehow, all the parts of our bodies keep us moving and living.',
   },
   {
     name: 'Relinquo',
@@ -108,6 +117,7 @@ export const LINE_INFO = [
       'Kwins Yang',
       'Jacky Tian'
     ],
+    video_ready: 'https://streamable.com/6oyxs4',
     description: 'The migration of the human race is undertaken by a series of obligatory adaptation, monotonous meandering, and warped reawakening. NORI: all energy resources are powered by water. NOMEN: the end of pigmentation. NOIR: storage and transportation of memories become achievable. NOMAD: functionalism and utilitarianism are main ideologies. NORRIN: a group of interstellar hipsters rises. NOAH: new myths and tales were created based on life on earth. NO.1379: a protest against the human race invading other civilizations. NOTO: women warriors become leaders of the human race.',
   },
   {
@@ -118,6 +128,7 @@ export const LINE_INFO = [
     ],
     description: 'Before 2020, we dreamed of an immersive, virtual future, of automata and instancy and the superhuman, but none of it was necessary. Today, as a result of the pandemic, we have finally gotten a taste of a virtual future — and it has notes of danger. Virtuality dictates our working, learning, socializing, and entertainment, harming our minds, bodies, and goals, and Digital Warfare is a call to revolt. We illustrate eight bright, dauntless, and forceful leaders in the movement, each representing a negative impact of technology, who seek a return and adherence to traditional reality. ',
     video_ready: 'https://streamable.com/4l78dp',
+    description: 'Before 2020, we dreamed of an immersive, virtual future, of automata and instancy and the superhuman, but none of it was necessary. Today, as a result of the pandemic, we have finally gotten a taste of a virtual future — and it has notes of danger. Virtuality dictates our working, learning, socializing, and entertainment, harming our minds, bodies, and goals, and Digital Warfare is a call to revolt. We illustrate eight bright, dauntless, and forceful leaders in the movement, each representing a negative impact of technology, who seek a return and adherence to traditional reality.',
   },
   {
     name: 'Icarus',
@@ -126,8 +137,18 @@ export const LINE_INFO = [
       'Leah Kendrick',
       'Tyng Peck'
     ],
+    video_ready: 'https://streamable.com/op8qo6',
     description: 'ICARUS creates a vision of post-apocalyptic, human architectures while exploring the evolution of solar shading and environmental protection systems. As global heating consumes landmasses and sediment pollutes airways, the call to shelter in place within one’s own personal space becomes real. While alarms blare in today’s hazmat suit, the future of skin and protection allows us to filter ultraviolet and pollution while living a lifestyle of the occasion. Through exploring and recreating iconic forms that emblemize and transcend class and gender, we created a world where beauty is isolation, mechanics, and self-obsession.',
   },
+  {
+    name: 'Alumni',
+    designers: [
+      'TBD'
+    ],
+    video_ready: 'https://streamable.com/3z2z9z',
+    description: 'Two talented alumni have returned to design the Alumni Line, a celebration of CMU graduates who we’ve stayed connected to. Our first alumna is Grace Kao, whose line will be androgynous and soft, pulled together by the color white, which will represent a blank slate and hopeful new beginning for 2021. Next, Terry Boyd will infuse the 90’s in his line of male streetwear, incorporating pieces from the Spice Girls, his first musical love. He will work in black and white, embroidery, and gold leaf. We are glad to have them back in the community and designing for Lunar Gala.',
+
+  }
 ];
 
 
